70594d20dd00d910dc22ac6435d12b4d1f87865a
kaushiktandon <kaushikt@usc.edu>
2019-04-23 20:07:07 -0700

    working tron!!!!

--- TRON.v
+++ TRON.v
@@ -1,12 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// VGA verilog template
-// Author:  Da Cheng
+// TRON
+// Author:  Kaushik Tandon, Grant Garcia
 //////////////////////////////////////////////////////////////////////////////////
 module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, btnU, btnD, btnL, btnR,
 	St_ce_bar, St_rp_bar, Mt_ce_bar, Mt_St_oe_bar, Mt_St_we_bar,
 	An0, An1, An2, An3, Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp,
-	LD0, LD1, LD2, LD3, LD4, LD5, LD6, LD7);
+	LD0, LD1, LD2, LD3, LD4, LD5, LD6, LD7, JA);
+	inout [7:0] JA;
 	input ClkPort, Sw0, btnU, btnD, btnL, btnR, Sw0, Sw1;
 	output St_ce_bar, St_rp_bar, Mt_ce_bar, Mt_St_oe_bar, Mt_St_we_bar;
 	output vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b;
@@ -45,60 +46,44 @@ module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, 
 	/////////////////////////////////////////////////////////////////
 	///////////////		VGA control starts here		/////////////////
 	/////////////////////////////////////////////////////////////////
+	reg R1 =0;
+	reg G1 =0;
+	reg R2 = 0;
+	reg G2 = 0;
+	reg R3 = 1;
+	reg G3 = 1;
 	
-	
-	
-	
-	
-
-	
-	reg R1;
-	reg R2=0;
-	reg	R3=0;
-	reg	R4=0;
-	reg	R5=0;
 	reg B1=0;
-	reg flag=1;
+
+	wire R;
+	wire G;
+	wire B;
 
 	
+	reg [9:0] P1p1xpos = 200;
+	reg [9:0] P1p1ypos = 200;
 	
-	wire R;//CounterY>=(100) && CounterY<=(200) && CounterX>=(100) && CounterX<=(200);//CounterY>=(yPosition-yPath) && CounterY<=(yPosition+yPath) && CounterX>=(xPosition-xPath) && CounterX<=(xPosition+xPath);
-	assign R=R1;
-	reg G = 0;//CounterX>100 && CounterX<200 && CounterY[8:5]==7;
-	wire B = B1;
+	reg [9:0] P2p2xpos = 400;
+	reg [9:0] P2p2ypos = 400;
 
-	
-	reg [9:0] P1xPos = 200;
-	reg [9:0] P1yPos = 200;
-	
-	reg [9:0] P2xPos = 10;
-	reg [9:0] P2yPos = 10;
-
-	reg [1:0] P1direction=2'b00;
-	reg [1:0] P2direction;
+	reg [1:0] P1direction=2'b01;
+	reg [1:0] P2direction=2'b11;
 	
 	//00 = up
 	//01 = right
 	//10 = down
 	//11 = left
 	
-	
 	reg P1win=0;
 	reg P2win=0;
 	
-	reg upperlim = 500;
-	reg blockSize = 20;
-	reg halfBlock = 10;
-	
-	
-		
-
+	reg upperlim = 480;
 	
 	always @(posedge DIV_CLK[21])
 		begin
 			if(reset)
 			begin
-				P1direction <= 2'b00;
+				P1direction <= 2'b01;
 			end
 			else if(btnD && ~btnU && ~btnL && ~btnR)
 			begin
@@ -117,163 +102,350 @@ module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, 
 				P1direction <= 2'b01;
 			end
 		end
-		
-		
+	always @(posedge DIV_CLK[21])
+		begin
+			if(reset)
+			begin
+				P2direction <= 2'b11;
+			end
+			//JA[0] down, JA[1] left, JA[2] right, JA[3] up
+			else if(JA[0] && ~JA[3] && ~JA[1] && ~JA[2])
+			begin
+				P2direction <= 2'b10;
+			end
+			else if(JA[3] && ~JA[0] && ~JA[1] && ~JA[2])
+			begin
+				P2direction <= 2'b00;
+			end
+			else if(JA[1] && ~JA[3] && ~JA[0] && ~JA[2])
+			begin
+				P2direction <= 2'b11;
+			end
+			else if(JA[2] && ~JA[3] && ~JA[0] && ~JA[1])
+			begin
+				P2direction <= 2'b01;
+			end
+		end
+			
+	reg [9:0] p1xpos[15:0];
+	reg [9:0] p1ypos[15:0];
 
-	reg [8:0] R3pos=10; 
-	reg R3flag=1;
-	reg [8:0] R4pos=100; 
-	reg R4flag=0;
-	reg [8:0] R5pos=10; 
-	reg R5flag=1;
-	reg [9:0] xpos[3:0];
-	reg [9:0] ypos[3:0];
-	
-	initial begin
-		xpos[3] = 200;
-		xpos[2] = 200;
-		xpos[1] = 200;
-		xpos[0] = 200;
-		
-		ypos[3] = 200;
-		ypos[2] = 200;
-		ypos[1] = 200;
-		ypos[0] = 200;
-	end
+	reg [9:0] p2xpos[15:0];
+	reg [9:0] p2ypos[15:0];
 	
 	integer i;
+	integer j;
 	
-	//assign R = (CounterY>=(ypos[3]-10) && CounterY<=(ypos[3]+10) && CounterX>=(xpos[3]-10) && CounterX<=(xpos[3]+10)) ||
-	//				(CounterY>=(ypos[2]-10) && CounterY<=(ypos[2]+10) && CounterX>=(xpos[2]-10) && CounterX<=(xpos[2]+10)) ||
-	//				(CounterY>=(ypos[1]-10) && CounterY<=(ypos[1]+10) && CounterX>=(xpos[1]-10) && CounterX<=(xpos[1]+10)) ||
-	//				(CounterY>=(ypos[0]-10) && CounterY<=(ypos[0]+10) && CounterX>=(xpos[0]-10) && CounterX<=(xpos[0]+10));
 	
+	initial begin
+		p1xpos[15] = 200;
+		p1xpos[14] = 200;
+		p1xpos[13] = 200;
+		p1xpos[12] = 200;
+		p1xpos[11] = 200;
+		p1xpos[10] = 200;
+		p1xpos[9] = 200;
+		p1xpos[8] = 200;
+		p1xpos[7] = 200;
+		p1xpos[6] = 200;
+		p1xpos[5] = 200;
+		p1xpos[4] = 200;
+		p1xpos[3] = 200;
+		p1xpos[2] = 200;
+		p1xpos[1] = 200;
+		p1xpos[0] = 200;
+		
+		p1ypos[15] = 200;
+		p1ypos[14] = 200;
+		p1ypos[13] = 200;
+		p1ypos[12] = 200;
+		p1ypos[11] = 200;
+		p1ypos[10] = 200;
+		p1ypos[9] = 200;
+		p1ypos[8] = 200;		
+		p1ypos[7] = 200;
+		p1ypos[6] = 200;
+		p1ypos[5] = 200;
+		p1ypos[4] = 200;
+		p1ypos[3] = 200;
+		p1ypos[2] = 200;
+		p1ypos[1] = 200;
+		p1ypos[0] = 200;
+		
+		p2xpos[15] = 400;
+		p2xpos[14] = 400;
+		p2xpos[13] = 400;
+		p2xpos[12] = 400;
+		p2xpos[11] = 400;
+		p2xpos[10] = 400;
+		p2xpos[9] = 400;
+		p2xpos[8] = 400;
+		p2xpos[7] = 400;
+		p2xpos[6] = 400;
+		p2xpos[5] = 400;
+		p2xpos[4] = 400;
+		p2xpos[3] = 400;
+		p2xpos[2] = 400;
+		p2xpos[1] = 400;
+		p2xpos[0] = 400;
+		
+		p2ypos[15] = 400;
+		p2ypos[14] = 400;
+		p2ypos[13] = 400;
+		p2ypos[12] = 400;
+		p2ypos[11] = 400;
+		p2ypos[10] = 400;
+		p2ypos[9] = 400;
+		p2ypos[8] = 400;		
+		p2ypos[7] = 400;
+		p2ypos[6] = 400;
+		p2ypos[5] = 400;
+		p2ypos[4] = 400;
+		p2ypos[3] = 400;
+		p2ypos[2] = 400;
+		p2ypos[1] = 400;
+		p2ypos[0] = 400;
+	end
+	
+	assign R = 		
+		(CounterY>=(p1ypos[15]-8) && CounterY<=(p1ypos[15]+8) && CounterX>=(p1xpos[15]-8) && CounterX<=(p1xpos[15]+8)) ||
+		(CounterY>=(p1ypos[14]-8) && CounterY<=(p1ypos[14]+8) && CounterX>=(p1xpos[14]-8) && CounterX<=(p1xpos[14]+8)) ||
+		(CounterY>=(p1ypos[13]-8) && CounterY<=(p1ypos[13]+8) && CounterX>=(p1xpos[13]-8) && CounterX<=(p1xpos[13]+8)) ||
+		(CounterY>=(p1ypos[12]-8) && CounterY<=(p1ypos[12]+8) && CounterX>=(p1xpos[12]-8) && CounterX<=(p1xpos[12]+8)) ||
+		(CounterY>=(p1ypos[11]-8) && CounterY<=(p1ypos[11]+8) && CounterX>=(p1xpos[11]-8) && CounterX<=(p1xpos[11]+8)) ||
+		(CounterY>=(p1ypos[10]-8) && CounterY<=(p1ypos[10]+8) && CounterX>=(p1xpos[10]-8) && CounterX<=(p1xpos[10]+8)) ||
+		(CounterY>=(p1ypos[9]-8) && CounterY<=(p1ypos[9]+8) && CounterX>=(p1xpos[9]-8) && CounterX<=(p1xpos[9]+8)) ||
+		(CounterY>=(p1ypos[8]-8) && CounterY<=(p1ypos[8]+8) && CounterX>=(p1xpos[8]-8) && CounterX<=(p1xpos[8]+8)) ||
+		(CounterY>=(p1ypos[7]-8) && CounterY<=(p1ypos[7]+8) && CounterX>=(p1xpos[7]-8) && CounterX<=(p1xpos[7]+8)) ||
+		(CounterY>=(p1ypos[6]-8) && CounterY<=(p1ypos[6]+8) && CounterX>=(p1xpos[6]-8) && CounterX<=(p1xpos[6]+8)) ||
+		(CounterY>=(p1ypos[5]-8) && CounterY<=(p1ypos[5]+8) && CounterX>=(p1xpos[5]-8) && CounterX<=(p1xpos[5]+8)) ||
+		(CounterY>=(p1ypos[4]-8) && CounterY<=(p1ypos[4]+8) && CounterX>=(p1xpos[4]-8) && CounterX<=(p1xpos[4]+8)) ||
+		(CounterY>=(p1ypos[3]-8) && CounterY<=(p1ypos[3]+8) && CounterX>=(p1xpos[3]-8) && CounterX<=(p1xpos[3]+8)) ||
+		(CounterY>=(p1ypos[2]-8) && CounterY<=(p1ypos[2]+8) && CounterX>=(p1xpos[2]-8) && CounterX<=(p1xpos[2]+8)) ||
+		(CounterY>=(p1ypos[1]-8) && CounterY<=(p1ypos[1]+8) && CounterX>=(p1xpos[1]-8) && CounterX<=(p1xpos[1]+8)) ||
+		(CounterY>=(p1ypos[0]-8) && CounterY<=(p1ypos[0]+8) && CounterX>=(p1xpos[0]-8) && CounterX<=(p1xpos[0]+8)) || R1 || R2;
+					
+	assign G = 			
+		(CounterY>=(p2ypos[15]-8) && CounterY<=(p2ypos[15]+8) && CounterX>=(p2xpos[15]-8) && CounterX<=(p2xpos[15]+8)) ||
+		(CounterY>=(p2ypos[14]-8) && CounterY<=(p2ypos[14]+8) && CounterX>=(p2xpos[14]-8) && CounterX<=(p2xpos[14]+8)) ||
+		(CounterY>=(p2ypos[13]-8) && CounterY<=(p2ypos[13]+8) && CounterX>=(p2xpos[13]-8) && CounterX<=(p2xpos[13]+8)) ||
+		(CounterY>=(p2ypos[12]-8) && CounterY<=(p2ypos[12]+8) && CounterX>=(p2xpos[12]-8) && CounterX<=(p2xpos[12]+8)) ||
+		(CounterY>=(p2ypos[11]-8) && CounterY<=(p2ypos[11]+8) && CounterX>=(p2xpos[11]-8) && CounterX<=(p2xpos[11]+8)) ||
+		(CounterY>=(p2ypos[10]-8) && CounterY<=(p2ypos[10]+8) && CounterX>=(p2xpos[10]-8) && CounterX<=(p2xpos[10]+8)) ||
+		(CounterY>=(p2ypos[9]-8) && CounterY<=(p2ypos[9]+8) && CounterX>=(p2xpos[9]-8) && CounterX<=(p2xpos[9]+8)) ||
+		(CounterY>=(p2ypos[8]-8) && CounterY<=(p2ypos[8]+8) && CounterX>=(p2xpos[8]-8) && CounterX<=(p2xpos[8]+8)) ||
+		(CounterY>=(p2ypos[7]-8) && CounterY<=(p2ypos[7]+8) && CounterX>=(p2xpos[7]-8) && CounterX<=(p2xpos[7]+8)) ||
+		(CounterY>=(p2ypos[6]-8) && CounterY<=(p2ypos[6]+8) && CounterX>=(p2xpos[6]-8) && CounterX<=(p2xpos[6]+8)) ||
+		(CounterY>=(p2ypos[5]-8) && CounterY<=(p2ypos[5]+8) && CounterX>=(p2xpos[5]-8) && CounterX<=(p2xpos[5]+8)) ||
+		(CounterY>=(p2ypos[4]-8) && CounterY<=(p2ypos[4]+8) && CounterX>=(p2xpos[4]-8) && CounterX<=(p2xpos[4]+8)) ||
+		(CounterY>=(p2ypos[3]-8) && CounterY<=(p2ypos[3]+8) && CounterX>=(p2xpos[3]-8) && CounterX<=(p2xpos[3]+8)) ||
+		(CounterY>=(p2ypos[2]-8) && CounterY<=(p2ypos[2]+8) && CounterX>=(p2xpos[2]-8) && CounterX<=(p2xpos[2]+8)) ||
+		(CounterY>=(p2ypos[1]-8) && CounterY<=(p2ypos[1]+8) && CounterX>=(p2xpos[1]-8) && CounterX<=(p2xpos[1]+8)) ||
+		(CounterY>=(p2ypos[0]-8) && CounterY<=(p2ypos[0]+8) && CounterX>=(p2xpos[0]-8) && CounterX<=(p2xpos[0]+8)) || G1 || G2;	
+
 	always @(posedge DIV_CLK[21])
 	begin
-			xpos[3]<=P1xPos;
-			xpos[2]<=xpos[3];
-			xpos[1]<=xpos[2];
-			xpos[0]<=xpos[1];
-			
-			
-			ypos[3]<=P1yPos;
-			ypos[2]<=ypos[3];
-			ypos[1]<=ypos[2];
-			ypos[0]<=ypos[1];
-			
-			//R2<=(CounterY>=(P1yPos-10) && CounterY<=(P1yPos+10) && CounterX>=(P1xPos-10) && CounterX<=(P1xPos+10));
-			
-			for(i=0;i<=3;i=i+1)
-			begin
-				R1 = (CounterY>=(ypos[i]-10) && CounterY<=(ypos[i]+10) && CounterX>=(xpos[i]-10) && CounterX<=(xpos[i]+10)) | R1;
-			end 
-			/*
-			R3<=(CounterY>=(290) && CounterY<=(310) && CounterX>=(R3pos-10) && CounterX<=(R3pos+10));
-			
-			R4<=(CounterY>=(90) && CounterY<=(110) && CounterX>=(R4pos-10) && CounterX<=(R4pos+10));
-						
-			R5<=(CounterY>=(R5pos-10) && CounterY<=(R5pos+10) && CounterX>=(90) && CounterX<=(110));
-			*/
-			//flag=0;
-			
-			
-			
-			B1<=(CounterY>=(200-10) && CounterY<=(200+10) && CounterX>=(200-10) && CounterX<=(200+10));
+		p1xpos[15]<=P1p1xpos;
+		p1xpos[14]<=p1xpos[15];
+		p1xpos[13]<=p1xpos[14];
+		p1xpos[12]<=p1xpos[13];
+		p1xpos[11]<=p1xpos[12];
+		p1xpos[10]<=p1xpos[11];
+		p1xpos[9]<=p1xpos[10];
+		p1xpos[8]<=p1xpos[9];
+		p1xpos[7]<=p1xpos[8];
+		p1xpos[6]<=p1xpos[7];
+		p1xpos[5]<=p1xpos[6];
+		p1xpos[4]<=p1xpos[5];
+		p1xpos[3]<=p1xpos[4];
+		p1xpos[2]<=p1xpos[3];
+		p1xpos[1]<=p1xpos[2];
+		p1xpos[0]<=p1xpos[1];			
 		
+		p1ypos[15]<=P1p1ypos;
+		p1ypos[14]<=p1ypos[15];
+		p1ypos[13]<=p1ypos[14];
+		p1ypos[12]<=p1ypos[13];
+		p1ypos[11]<=p1ypos[12];
+		p1ypos[10]<=p1ypos[11];
+		p1ypos[9]<=p1ypos[10];
+		p1ypos[8]<=p1ypos[9];
+		p1ypos[7]<=p1ypos[8];
+		p1ypos[6]<=p1ypos[7];
+		p1ypos[5]<=p1ypos[6];
+		p1ypos[4]<=p1ypos[5];
+		p1ypos[3]<=p1ypos[4];
+		p1ypos[2]<=p1ypos[3];
+		p1ypos[1]<=p1ypos[2];
+		p1ypos[0]<=p1ypos[1];
+		
+	end
+	always @(posedge DIV_CLK[21])
+	begin
+		p2xpos[15]<=P2p2xpos;
+		p2xpos[14]<=p2xpos[15];
+		p2xpos[13]<=p2xpos[14];
+		p2xpos[12]<=p2xpos[13];
+		p2xpos[11]<=p2xpos[12];
+		p2xpos[10]<=p2xpos[11];
+		p2xpos[9]<=p2xpos[10];
+		p2xpos[8]<=p2xpos[9];
+		p2xpos[7]<=p2xpos[8];
+		p2xpos[6]<=p2xpos[7];
+		p2xpos[5]<=p2xpos[6];
+		p2xpos[4]<=p2xpos[5];
+		p2xpos[3]<=p2xpos[4];
+		p2xpos[2]<=p2xpos[3];
+		p2xpos[1]<=p2xpos[2];
+		p2xpos[0]<=p2xpos[1];			
+		
+		p2ypos[15]<=P2p2ypos;
+		p2ypos[14]<=p2ypos[15];
+		p2ypos[13]<=p2ypos[14];
+		p2ypos[12]<=p2ypos[13];
+		p2ypos[11]<=p2ypos[12];
+		p2ypos[10]<=p2ypos[11];
+		p2ypos[9]<=p2ypos[10];
+		p2ypos[8]<=p2ypos[9];
+		p2ypos[7]<=p2ypos[8];
+		p2ypos[6]<=p2ypos[7];
+		p2ypos[5]<=p2ypos[6];
+		p2ypos[4]<=p2ypos[5];
+		p2ypos[3]<=p2ypos[4];
+		p2ypos[2]<=p2ypos[3];
+		p2ypos[1]<=p2ypos[2];
+		p2ypos[0]<=p2ypos[1];
 	end
 	
 	
-	
 	always @(posedge DIV_CLK[21])
 	begin
-		/*
 		if(reset)
 		begin
-			P1xPos <= 25;
-			P1yPos <= 25;
+			P1p1xpos <= 200;
+			P1p1ypos <= 200;
 		end
-		*/
-		if(R3flag==1)
-				R3pos<=R3pos+4;
-			else
-				R3pos<=R3pos-4;
-			if(R3pos>399)
-			begin
-				R3flag=0;
-			end
-			if(R3pos<9)
-				R3flag=1;
-		if(R4flag==1)
-				R4pos<=R4pos+4;
-			else
-				R4pos<=R4pos-4;
-			if(R4pos>399)
-			begin
-				R4flag=0;
-			end
-			if(R4pos<9)
-				R4flag=1;
-				
-				
-				
-		if(R5flag==1)
-				R5pos<=R5pos+4;
-			else
-				R5pos<=R5pos-4;
-			if(R5pos>399)
-			begin
-				R5flag=0;
-			end
-			if(R5pos<9)
-				R5flag=1;
-				
 				
 		if(P1direction == 2'b00)
 		begin
-			P1yPos<=P1yPos-20;
-			
-			if(P1yPos>upperlim || P1yPos<1)
+			P1p1ypos<=P1p1ypos-5;
+			if(P1p1ypos>upperlim || P1p1ypos<1)
 			begin
-				P2win<=1;
+				//G2<=1;
 			end
 			
 			
 		end
 		else if(P1direction == 2'b01)
 		begin
-			P1xPos <= P1xPos + 20;
-			if(P1xPos>upperlim || P1xPos<1)
+			P1p1xpos <= P1p1xpos + 5;
+			if(P1p1xpos>upperlim || P1p1xpos<1)
 			begin
-				P2win<=1;
+				//G2<=1;
 			end
 			
 			
 		end
 		else if(P1direction == 2'b10)
 		begin
-			P1yPos<=P1yPos+20;
-			if(P1yPos>upperlim || P1yPos<1)
+			P1p1ypos<=P1p1ypos+5;
+			if(P1p1ypos>upperlim || P1p1ypos<1)
 			begin
-				P2win<=1;
+				//G2<=1;
 			end
 			
 		end
 		else if(P1direction == 2'b11)
 		begin
-			P1xPos <= P1xPos - 20;
-			if(P1xPos>upperlim || P1xPos<1)
+			P1p1xpos <= P1p1xpos - 5;
+			if(P1p1xpos>upperlim || P1p1xpos<1)
 			begin
-				P2win<=1;
+				//G2<=1;
+			end
+			
+		end
+	end
+
+	always @(posedge DIV_CLK[21])
+	begin
+		if(reset)
+		begin
+			P2p2xpos <= 400;
+			P2p2ypos <= 400;
+		end
+				
+		if(P2direction == 2'b00)
+		begin
+			P2p2ypos<=P2p2ypos-5;
+			
+			if(P2p2ypos>upperlim || P2p2ypos<1)
+			begin
+				//R2<=1;
+			end
+			
+			
+		end
+		else if(P2direction == 2'b01)
+		begin
+			P2p2xpos <= P2p2xpos + 5;
+			if(P2p2xpos>upperlim || P2p2xpos<1)
+			begin
+				//R2<=1;
+			end
+			
+			
+		end
+		else if(P2direction == 2'b10)
+		begin
+			P2p2ypos<=P2p2ypos+5;
+			if(P2p2ypos>upperlim || P2p2ypos<1)
+			begin
+				//R2<=1;
+			end
+			
+		end
+		else if(P2direction == 2'b11)
+		begin
+			P2p2xpos <= P2p2xpos - 5;
+			if(P2p2xpos>upperlim || P2p2xpos<1)
+			begin
+				//R2<=1;
 			end
 			
 		end
 	end
 	
+
+	always @(posedge DIV_CLK[21])
+	begin
+		for(i = 0; i <= 15; i = i + 1)
+		begin
+			if((p1xpos[i] >= P2p2xpos - 10) && (p1xpos[i] <= P2p2xpos + 10) && (p1ypos[i] >= P2p2ypos - 10) && (p1ypos[i] <= P2p2ypos + 10))
+			begin
+				R1 = 1;
+				G3 = 0;
+			end
+		end
+	end
+	
+	always @(posedge DIV_CLK[21])
+	begin
+		for(j = 0; j <= 15; j = j + 1)
+		begin
+			if((p2xpos[j] >= P1p1xpos - 10) && (p2xpos[j] <= P1p1xpos + 10) && (p2ypos[j] >= P1p1ypos - 10) && (p2ypos[j] <= P1p1ypos + 10))
+			begin
+				G1 = 1;
+				R3 = 0;
+			end
+		end
+	end
+	
+	
 	
 	always @(posedge clk)
 	begin
-		vga_r <= R & inDisplayArea;
-		vga_g <= G & inDisplayArea;
+		vga_r <= R & R3 & inDisplayArea;
+		vga_g <= G & G3 & inDisplayArea;
 		vga_b <= B & inDisplayArea;
 	end
 	
@@ -319,7 +491,7 @@ module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, 
 	assign SSD3 = 4'b1111;
 	assign SSD2 = 4'b1111;
 	assign SSD1 = 4'b1111;
-	assign SSD0 = P1yPos[3:0];
+	assign SSD0 = P1p1ypos[3:0];
 	
 	// need a scan clk for the seven segment display 
 	// 191Hz (50MHz / 2^18) works well
